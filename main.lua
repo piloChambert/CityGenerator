4c9769faac875838e64872fdaa8a577765a6221b
PLCGame <pierrelaurent.chambert@gmail.com>
2016-10-06 17:13:13 +0200

    Updated constraints

--- main.lua
+++ main.lua
@@ -26,6 +26,10 @@ function Segment.new(pos, dir, len)
 	return self
 end
 
+function Segment:endPoint() 
+	return self.pos + self.dir * self.length
+end
+
 setmetatable(Segment, { __call = function(_, ...) return Segment.new(...) end })
 
 Road = {}
@@ -45,7 +49,7 @@ setmetatable(Road, { __call = function(_, ...) return Road.new(...) end })
 
 -- configuration
 roadsParameters = {
-	maxRoads = 1000,
+	maxRoads = 1000000,
 
 	-- highway
 	highwayLength = 150,
@@ -54,9 +58,11 @@ roadsParameters = {
 	highwayLookUpRayStepCount = 20,
 	highwayLookUpRayStepLength = 20000,
 	highwayConnectionDistance = 40, 
+	highwayPriority = 1,
 
 	-- streets
-	streetLength = 150
+	streetLength = 100,
+	streetPriority = 200
 }
 
 -- return the population at a point
@@ -71,16 +77,14 @@ function intersectSegment(A, B)
 	local den = A.dir * B.dir
 
 	if math.abs(den) > 0.0 then
-		-- I = A.pos + B.dir * u = B.pos + B.dir * v
+		-- I = A.pos + A.dir * u = B.pos + B.dir * v
 		local diff = B.pos - A.pos
 
 		local u = (diff * B.dir) / den
 		local v = (diff * A.dir) / den
 
 		-- intersection
-		if u > 0.00001 and u < A.length and v > 0.0001 and v < B.length then
-			return true, u, v
-		end
+		return true, u, v
 	end
 
 	return false, nil, nil
@@ -91,16 +95,24 @@ function localConstraints(road)
 
 	if road.attr.highway then
 		segment.width = 8.0
+		segment.color = {r = 255, g = 255, b = 255}
+	else
+		segment.color = {r = 64, g = 64, b = 64}		
 	end
 
 	-- PRUNNING
-	-- if there's a segment connected and almost the same direction
-	-- don't geneate a road
+	-- discard segment if there's one to close with the same orientation
 	for other in segments:iterate() do
-		if (segment.pos - other.pos):length() < 20.0 and math.abs(segment.dir:dot(other.dir)) > 0.6 then
-			return false
+		if math.abs(other.dir:dot(segment.dir)) > 0.9 then
+			local d0 = math.max((segment.pos - other.pos):length(), (segment.pos - other:endPoint()):length())
+			local d1 = math.max((segment:endPoint() - other.pos):length(), (segment:endPoint() - other:endPoint()):length())			
+
+			if math.max(d0, d1) < segment.length then
+				return false
+			end
 		end
 	end
+
 
 	-- RULE 1
 	-- itersection with other segment
@@ -110,7 +122,7 @@ function localConstraints(road)
 		local intersect, u, v = intersectSegment(segment, other)
 
 		-- intersection
-		if intersect then
+		if intersect and u > 0.0001 and v > 0.0001 and u < segment.length and v < other.length then
 			if u < closestIntersectionDistance then
 				closestIntersectionDistance = u
 				closestSegment = other
@@ -125,13 +137,14 @@ function localConstraints(road)
 		local E = closestSegment.pos + closestSegment.dir * closestSegment.length -- end of splited segment
 
 		local s0 = Segment(closestSegment.pos, (I - closestSegment.pos):normalized(), (I - closestSegment.pos):length())
-		s0.color = {r = 0, g = 255, b = 0}
+		s0.color = closestSegment.color
 		s0.width = closestSegment.width
 
 		local s1 = Segment(I, (E - I):normalized(), (E - I):length())
-		s1.color = {r = 0, g = 255, b = 0}
+		s1.color = closestSegment.color
 		s1.width = closestSegment.width
 
+		-- remove splitte edge
 		segments:remove(closestSegment)
 		segments:push(s0)
 		segments:push(s1)
@@ -141,21 +154,26 @@ function localConstraints(road)
 		segment.length = closestIntersectionDistance
 		segment.color = {r = 0, g = 0, b = 255}
 
+		-- doesn't expand the road
+		road.attr.done = true
+
 		return true
 	end
 
 	-- RULE 2 
 	-- else check closest existing intersection
-	local P = segment.pos + segment.dir * segment.length
+	local P = segment:endPoint() 
 	closestIntersectionDistance = roadsParameters.highwayConnectionDistance
 	local closestIntersection = nil
 	for other in segments:iterate() do
 		if (P - other.pos):length() < closestIntersectionDistance then
 			closestIntersectionDistance = (P - other.pos):length()
 			closestIntersection = other.pos
-		elseif (P - (other.pos + other.dir * other.length)):length() < closestIntersectionDistance then
-			closestIntersectionDistance = (P - (other.pos + other.dir * other.length)):length()
-			closestIntersection = other.pos + other.dir * other.length
+		end
+
+		if (P - other:endPoint()):length() < closestIntersectionDistance then
+			closestIntersectionDistance = (P - other:endPoint()):length()
+			closestIntersection = other:endPoint()
 		end
 	end
 
@@ -164,15 +182,63 @@ function localConstraints(road)
 		segment.dir = V:normalized()
 		segment.length = V:length()
 		segment.color = {r = 255, g = 255, b = 0}
+
+		-- doesn't expand the road
+		road.attr.done = true
+
+		return true
 	end
 
 	-- RULE 3
+	-- check distance to the closest segment
+	closestIntersectionDistance = 100--roadsParameters.highwayConnectionDistance
+	closestSegment = nil
+	for other in segments:iterate() do
+		local intersect, u, v = intersectSegment(segment, other)
 
-	-- color
-	if road.attr.highway then
-		segment.color = {r = 255, g = 255, b = 255}
-	else
-		segment.color = {r = 127, g = 127, b = 127}		
+		-- intersection
+		if intersect and u > 0.0001 and v > 0.0001 and v < other.length then
+			if u < closestIntersectionDistance then
+				closestIntersectionDistance = u
+				closestSegment = other
+			end
+		end
+	end
+
+	if closestSegment ~= nil then
+		-- create an itersection here
+		-- split road
+		local I = segment.pos + segment.dir * closestIntersectionDistance
+		local E = closestSegment:endPoint() -- end of splited segment
+
+		local s0 = Segment(closestSegment.pos, (I - closestSegment.pos):normalized(), (I - closestSegment.pos):length())
+		s0.color = {r = 0, g = 255, b = 255}
+		s0.width = closestSegment.width
+
+		local s1 = Segment(I, (E - I):normalized(), (E - I):length())
+		s1.color = {r = 0, g = 255, b = 255}
+		s1.width = closestSegment.width
+
+		-- remove splitte edge
+		segments:remove(closestSegment)
+		segments:push(s0)
+		segments:push(s1)
+
+
+		-- correct this road segment
+		segment.length = closestIntersectionDistance
+		segment.color = {r = 255, g = 0, b = 255}
+
+		-- doesn't expand the road
+		road.attr.done = true
+
+		return true
+	end
+
+	-- don't extend segments outside area
+	local E = segment:endPoint()
+	if math.abs(E.x) > 8192 or math.abs(E.y) > 8192 then
+		road.attr.done = true
 	end
 
 	return true
@@ -209,62 +275,55 @@ end
 function globalGoals(queue, t, road)
 	local segment = road.segment
 	local attr = road.attr
-	local newPosition = segment.pos + segment.dir * segment.length
+	local newPosition = segment:endPoint()
 	local dir = segment.dir
 	local currentPop = populationAt(newPosition.x, newPosition.y)
 
+	-- don't expand the road :)
+	if attr.done then
+		return
+	end
+
 	if attr.highway then
 		-- split?
+		local split = 1500
 		local leftSplit = attr.leftSplit - roadsParameters.highwayLength
 		local rightSplit = attr.rightSplit - roadsParameters.highwayLength
 
 		if attr.leftSplit < 0 and love.math.random() < 0.1 then
-			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * 0.5), roadsParameters.highwayLength), {highway = true, leftSplit = 2000, rightSplit = 2000}), t + 20)
-			leftSplit = 2000		
+			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * 0.5), roadsParameters.highwayLength), {highway = true, leftSplit = split, rightSplit = split}), t + roadsParameters.highwayPriority)
+			leftSplit = split		
 		end
 
 		if attr.rightSplit < 0 and love.math.random() < 0.1 then
-			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * -0.5), roadsParameters.highwayLength), {highway = true, leftSplit = 2000, rightSplit = 2000}), t + 20)
-			rightSplit = 2000
+			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * -0.5), roadsParameters.highwayLength), {highway = true, leftSplit = split, rightSplit = split}), t + roadsParameters.highwayPriority)
+			rightSplit = split
 		end
 
 		-- look for next population
 		local bestAngle = math.max(-roadsParameters.highwayAngleRange, math.min(roadsParameters.highwayAngleRange, bestDirection(newPosition, dir, math.pi * 0.1)))
 
 		-- push the best angle
-		queue:push(Road(Segment(newPosition, dir:rotated(bestAngle), roadsParameters.highwayLength), {highway = true, leftSplit = leftSplit, rightSplit = rightSplit }), t+20)
-
-		if currentPop > 0.0 then
-			-- generate streets
-			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * 0.5), roadsParameters.streetLength), {}), t + 60)		
-			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * -0.5), roadsParameters.streetLength), {}), t + 60)					
-		end
-	else
-		if currentPop > 0.0 then
-			-- keep going forward
-			queue:push(Road(Segment(newPosition, dir, roadsParameters.streetLength), {}), t + 60)
-		end
-
-		-- 90° intersection
-		queue:push(Road(Segment(newPosition, dir:rotated(math.pi * 0.5), roadsParameters.streetLength * 0.5), {}), t + 60)		
-		queue:push(Road(Segment(newPosition, dir:rotated(math.pi * -0.5), roadsParameters.streetLength * 0.5), {}), t + 60)		
+		queue:push(Road(Segment(newPosition, dir:rotated(bestAngle), roadsParameters.highwayLength), {highway = true, leftSplit = leftSplit, rightSplit = rightSplit }), t+roadsParameters.highwayPriority)
+	elseif currentPop > 0.0 then
+		-- keep forward
+		queue:push(Road(Segment(newPosition, dir, roadsParameters.streetLength), {}), t+roadsParameters.streetPriority)
 	end
-	
 
+	-- street branching
+	if currentPop > 0.0 then
+		if math.random() < 0.1 then
+			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * -0.5), roadsParameters.streetLength), {}), t+roadsParameters.streetPriority + 2)
+		end
 
+		if math.random() < 0.1 then
+			queue:push(Road(Segment(newPosition, dir:rotated(math.pi * 0.5), roadsParameters.streetLength), {}), t+roadsParameters.streetPriority + 2)
+		end
+	end
 end
 
-function love.load()
-	-- setup screen
-	love.window.setMode(1280, 720)
-
-	segments = list()
-	local queue = PriorityQueue.new()
-
-	local startAngle = bestDirection(Vector(0, 0), Vector(1, 0), math.pi)
-	queue:push(Road(Segment(Vector(0, 0), Vector(1, 0):rotated(startAngle), roadsParameters.highwayLength), {highway = true, leftSplit = 500, rightSplit = 500}) , 0)
-
-	while #queue > 0 and segments.length < roadsParameters.maxRoads do
+function step()
+	if #queue > 0 and segments.length < roadsParameters.maxRoads then
 		local road, t = queue:pop()
 		local accepted = localConstraints(road)
 
@@ -277,10 +336,40 @@ function love.load()
 	end
 end
 
+function love.load()
+	-- setup screen
+	love.window.setMode(1280, 720)
+
+	segments = list()
+	queue = PriorityQueue.new()
+
+	local startAngle = bestDirection(Vector(0, 0), Vector(1, 0), math.pi)
+	queue:push(Road(Segment(Vector(0, 0), Vector(1, 0):rotated(startAngle), roadsParameters.highwayLength), {highway = true, leftSplit = 500, rightSplit = 500}) , 0)
+	queue:push(Road(Segment(Vector(0, 0), Vector(-1, 0):rotated(startAngle), roadsParameters.highwayLength), {highway = true, leftSplit = 500, rightSplit = 500}) , 0)
+
+--[[
+	while #queue > 0 and segments.length < roadsParameters.maxRoads do
+		local road, t = queue:pop()
+		local accepted = localConstraints(road)
+
+		if accepted then
+			segments:push(road.segment)
+
+			-- add global goals
+			globalGoals(queue, t + 1, road)
+		end
+	end
+	]]
+end
+
 function love.update(dt)
 	-- quit
 	if love.keyboard.isDown("escape") then
 		love.event.quit()
+	end
+
+	for i = 1, 10 do
+		step()
 	end
 end
 
@@ -308,16 +397,18 @@ function love.draw()
 	--drawMap()
 
 	local w, h = love.window.getDimensions()
+	local cx = w * 0.5 - cameraX * zoom
+	local cy = h * 0.5 - cameraY * zoom
 	for y = 0, math.ceil(h / 16) do
 		for x = 0, math.ceil(w / 16) do
-			local n = populationAt(x * 16 / zoom + cameraX, y * 16 / zoom + cameraY)
+			local n = populationAt((x * 16 - cx) / zoom, (y * 16 - cy) / zoom)
 			love.graphics.setColor(n * 255, n * 255 * 0.2, n * 255 * 0.2, 255)
 			love.graphics.rectangle("fill", x * 16 - 8, y * 16 - 8, 16, 16)
 		end
 	end
 
 	love.graphics.push()
-	love.graphics.translate(-cameraX * zoom, -cameraY * zoom)
+	love.graphics.translate(cx, cy)
 	love.graphics.scale(zoom, zoom)
 	for seg in segments:iterate() do
 		love.graphics.setColor(seg.color.r, seg.color.g, seg.color.b, 255)
@@ -326,4 +417,9 @@ function love.draw()
 	end
 	
 	love.graphics.pop()
+
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.print("Segments: " .. segments.length, 10, 0)
+	love.graphics.print("Queue size: " .. #queue, 10, 15)
+
 end
